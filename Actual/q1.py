6d7eefae319683fffbfb62b9a5320b44b9a64691
simshengqin <sqsim.2018@sis.smu.edu.sg>
2021-02-26 02:19:34 +0800

    Add q1.py

--- Actual/q1.py
+++ Actual/q1.py
@@ -1,10 +1,14 @@
 import sys
 import math
-sys.stdin = open("a_example", 'r')
-[M, T2, T3, T4] =  list(map(int, input().split()))
-print(M,T2,T3,T4)
-pizzas = []
-for i in range(M):
-    pizzas.append(tuple(input().split()))
-
-
+sys.stdin = open("example.txt", 'r')
+[D,I,S,V,F] =  list(map(int, input().split()))
+intersections = {}
+cars = []
+for i in range(S):
+    [B,E,street_name,L] =  list(input().split())
+    intersections[street_name] = [B,E,L]
+for i in range(V):
+    arr =  list(input().split())
+    cars.append(arr)
+print(intersections)
+print(cars)
