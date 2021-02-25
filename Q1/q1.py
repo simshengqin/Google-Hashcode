772e62b626c12bc97da5131554928b51fc15646c
simshengqin <sqsim.2018@sis.smu.edu.sg>
2021-02-26 00:56:24 +0800

    update q1

--- Q1/q1.py
+++ Q1/q1.py
@@ -3,7 +3,42 @@ import math
 sys.stdin = open("a_example", 'r')
 [M, T2, T3, T4] =  list(map(int, input().split()))
 print(M,T2,T3,T4)
-pizzas = set()
+pizzas = []
 for i in range(M):
-    pizzas.add(tuple(input().split()))
+    pizzas.append(tuple(input().split()))
+two_combi = []
+for i in range(len(pizzas)):
+    for j in range(i, len(pizzas)):
+        #if i != j:
+            pizza1 = pizzas[i]
+            pizza2 = pizzas[j]
+            two_combi.append([len(set(pizza1[1:] + pizza2[1:])),(i, j)])
+three_combi = []
+for i in range(len(pizzas)):
+    for j in range(i, len(pizzas)):
+        for k in range(j, len(pizzas)):
+            # if i != j and j!= k and i != k:
+            pizza1 = pizzas[i]
+            pizza2 = pizzas[j]
+            pizza3 = pizzas[k]
+            three_combi.append([len(set(pizza1[1:] + pizza2[1:] + pizza3[1:])),(i, j, k)])
+four_combi = []
+for i in range(len(pizzas)):
+    for j in range(i, len(pizzas)):
+        for k in range(j, len(pizzas)):
+            for x in range(k, len(pizzas)):
+                pizza1 = pizzas[i]
+                pizza2 = pizzas[j]
+                pizza3 = pizzas[k]
+                pizza4 = pizzas[x]
+                four_combi.append([len(set(pizza1[1:] + pizza2[1:] + pizza3[1:] + pizza4[1:])),(i, j, k, x)])
 print(pizzas)
+print(two_combi)
+print()
+print()
+print(three_combi)
+print()
+print()
+print(four_combi)
+print()
+print()
